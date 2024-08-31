8cb1be86405ad67e426a06c92450b444443bd969
Stephen Leonardo <stephen.leonardo@grabtaxi.com>
2024-09-01 06:17:23 +0700

    hide summary if 0 times saved

--- screen-tracker/src/popup.js
+++ screen-tracker/src/popup.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   chrome.runtime.sendMessage({ action: 'getData' }, (response) => {
     if (response && response.data) {
-        document.getElementById('data-display').innerHTML = `
+         let popupContentHtml = `
           <div class="flex flex-col items-center p-8 bg-gradient-to-r from-blue-50 to-white shadow-lg rounded-lg">
           <!-- Title section -->
           <h1 class="text-3xl font-extrabold text-gray-900 mb-6">Topics & Categories</h1>
@@ -22,14 +22,15 @@ document.addEventListener("DOMContentLoaded", (e) => {
             ${response.data.topics.map(topic => `<span class="whitespace-nowrap mb-2 bg-red-300 text-white px-4 py-2 rounded-full shadow-lg transition-all duration-300 hover:shadow-xl hover:bg-red-400 cursor-pointer">${topic}</span>`).join('')}
             </div>
           </div>
-
-          <!-- Summary section -->
-          <div class="w-full max-w-md mt-6 p-6 bg-gray-100 rounded-lg shadow-lg">
+        `;
+        if (response.data.minutes_saved > 0) {
+          popupContentHtml += `<div class="w-full max-w-md mt-6 p-6 bg-gray-100 rounded-lg shadow-lg">
             <h2 class="text-lg font-semibold text-gray-800 mb-4">Summary:</h2>
             <p class="text-gray-700 text-base">${response.data.summary}</p>
-          </div>
-        </div>
-        `;
+          </div>`
+        }
+        popupContentHtml += `</div>`
+        document.getElementById('data-display').innerHTML = popupContentHtml
     }
   });
   
